acced7179f9fb66a477f7f9d985b928a89c1c0bd
vvk130 <vanessa.kasula@outlook.com>
2024-10-10 13:14:17 +0300

    Serve swagger at root, refator sieveprocessors to a folder

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ using TripNetReactBackend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.Configure<SieveOptions>(builder.Configuration.GetSection("Sieve"));
@@ -20,7 +19,6 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString(@"DefaultConnection")));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -32,12 +30,13 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Expose swagger because this is a portfolio project
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; 
+    });
 
 
 app.UseAuthorization();
