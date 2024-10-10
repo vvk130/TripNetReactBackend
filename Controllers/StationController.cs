3120a5c9a2710f6189f2e65cbeab14f4d8c7f271
vvk130 <vanessa.kasula@outlook.com>
2024-10-10 11:07:16 +0300

    Cleaning up comments

--- Controllers/StationController.cs
+++ Controllers/StationController.cs
@@ -29,19 +29,4 @@ public class StationController : ControllerBase
         models = _sieveprosessor.Apply(sieveModel, models, applyFiltering: false, applySorting: false);
         return Ok(models);
     }
-
-    // var stations = _context.Stations.ProjectTo<StationDto>(_mapper.ConfigurationProvider);
-    // stations = _sieveprosessor.Apply(sieveModel, models, applyPagination: false);
-    //     Request.HttpContext.Response.Headers.Append("X-Total-Count", models.Count().ToString());
-    //     models = _sieveprosessor.Apply(sieveModel, models, applyFiltering: false, applySorting: false);
-    //     return Ok(stations);
-
-    // [HttpGet("{id}")]
-    // public IActionResult GetStationById([FromQuery] int id)
-    // {
-
-    //     var stations = _context.Stations.ProjectTo<StationDto>(_mapper.ConfigurationProvider);
-    //     stations = _sieveprosessor.Apply(sieveModel, stations);
-    //     return Ok(pageTotal,stations);
-    // }
 }
