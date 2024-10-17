4c38f5aff13fad351c2a56e925c6a13b1ea106ae
vvk130 <vanessa.kasula@outlook.com>
2024-10-17 18:52:24 +0300

    Longitude and latitude error messages

--- Dtos/StationDto.cs
+++ Dtos/StationDto.cs
@@ -24,8 +24,8 @@ public record StationDto
         {
             RuleFor(t => t.StationName).MaximumLength(100);
             RuleFor(t => t.StationAddress).MaximumLength(100);
-            RuleFor(s => s.CoordinateY).Matches(@"^-? ([0 - 9]{1,2}| 1[0 - 7][0 - 9] | 180)(\.[0 - 9]{ 1,16})$");
-            RuleFor(s => s.CoordinateX).Matches(@"^-?([0-8]?[0-9]|90)(\.[0-9]{1,16})$");
+            RuleFor(s => s.CoordinateX).Matches(@"^-? ([0 - 9]{1,2}| 1[0 - 7][0 - 9] | 180)(\.[0 - 9]{ 1,16})$").WithMessage("Y coordinate needs to be between –180 to 180, and can have up to 16 decimals");
+            RuleFor(s => s.CoordinateY).Matches(@"^-?([0-8]?[0-9]|90)(\.[0-9]{1,16})$").WithMessage("X coordinate needs to be between –90 to 90, and can have up to 16 decimals");
         }
     }
 
